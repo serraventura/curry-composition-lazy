198dcb41b3c90516e5cffd9e07d7ee868ed585e5
Thiago Ventura <serraventura@gmail.com>
2019-07-11 18:07:49 +0100

    tests added

--- lazy.test.js
+++ lazy.test.js
@@ -1,11 +1,26 @@
 const Lazy = require("./lazy");
 
+// there are more cases that could be tested
+// I will ignore them since it is just a test
+
 describe("Lazy", () => {
-  it("add() - should add func", () => {
-    expect(true).toBeTruthy();
-  });
-  it("evaluate() - should add func", () => {
-    expect(true).toBeTruthy();
+  it("add() - should add func to the list", () => {
+    const lazy = new Lazy();
+    lazy.add(() => {}).add(() => {});
+    expect(lazy.arrFunc).not.toBeUndefined();
+    expect(lazy.arrFunc.length).toEqual(2);
   });
 
+  it("evaluate() - should return right calculation for a list of values", () => {
+    const lazy = new Lazy();
+    lazy.add(a => a + 1).add(a => a * 100);
+    expect(lazy.evaluate([1])).toEqual([200]);
+    expect(lazy.evaluate([1, 10])).toEqual([200, 1100]);
+  });
+
+  it("compose() - should return right calculation for a specific value", () => {
+    const lazy = new Lazy();
+    lazy.add(a => a + 1);
+    expect(lazy.compose(1)).toEqual(2);
+  });
 });
