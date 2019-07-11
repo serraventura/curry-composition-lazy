1f00a67fbf908d59c67803b76a2f8237140a247b
Thiago Ventura <serraventura@gmail.com>
2019-07-11 17:54:01 +0100

    finally jest babel configuration

---
+++ lazy.test.js
@@ -0,0 +1,11 @@
+const Lazy = require("./lazy");
+
+describe("Lazy", () => {
+  it("add() - should add func", () => {
+    expect(true).toBeTruthy();
+  });
+  it("evaluate() - should add func", () => {
+    expect(true).toBeTruthy();
+  });
+
+});
